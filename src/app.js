cd68b32e955130d97865bb92cc90f2ef60b55f2c
Jesse Hu <huyouare@gmail.com>
2014-10-04 17:39:12 -0700

    Final changes for Myo integration

--- src/app.js
+++ src/app.js
@@ -6,56 +6,75 @@ var webSocket = require('ws'),
     board = new five.Board(),
     SerialPort = require("serialport").SerialPort,
     serialPort = new SerialPort("/dev/cu.usbmodem14141", {
-		baudrate: 57600
-	}),
-	led, frame, pitch, roll, hand_normal;
+    baudrate: 57600
+  }),
+  led, frame, pitch, roll, hand_normal;
 
-	var sensitivity_constant = -100;
+  var sensitivity_constant = -100;
 
-	var out = [0, 0, 0];
+  var out = [0, 0, 0];
 
-  var euler = [0, 0, 0];
+  var euler = {};
 
 var initMyo = function(sensitivity_constant) {
-        "use strict";
+    "use strict";
 
-        window.hub = new Myo.Hub();
+    var Hub = require("./Hub");
+    var hub = new Hub();
 
-        window.hub.on('frame', function(frame) {
+    hub.on('frame', function(frame) {
 
-            euler[0] = frame.euler.y;
-            euler[1] = frame.euler.z;
-            euler[2] = frame.euler.x;
+        euler = frame.euler;
+        console.log(euler);
+        
+        // console.log(euler.pitch);
+        // console.log(euler.roll);
 
-        });
-    };
-initMyo();
+        pitch = Math.round(euler.pitch * -170 + 500);
+
+        roll = Math.round(euler.roll * 100 + 500);
+
+        console.log(pitch);
+        console.log(roll);
+
+
+        serialPort.write("1" + pitch + "\r");
+
+        serialPort.write("0" + roll + "\r");
+    });
+};
+initMyo(sensitivity_constant);
 
 var socketWrite = function(sensitivity_constant) {
-	mvg_avg = [0,0,0];    
+  mvg_avg = [0,0,0];
     ws.on('message', function(data, flags) {
        //  frame = JSON.parse(data); 
 
        //  if(frame.hands && frame.hands.length > 0) {
 
-       //  	hand_normal = frame.hands[0].palmNormal;
+       // hand_normal = frame.hands[0].palmNormal;
 
-	     	// console.log('Hand Normal: ' + hand_normal); // [roll, thumb_thing, pitch]
-	 		
-	    	pitch = Math.round(euler[0]*sensitivity_constant + 500);	
+        // console.log('Hand Normal: ' + hand_normal); // [roll, thumb_thing, pitch]
 
-	    	roll = Math.round(euler[2]*sensitivity_constant + 500);
+        // console.log(euler);
+      
+        // pitch = Math.round(euler.pitch*sensitivity_constant + 500);
 
-	    	serialPort.write("1" + roll + "\r");
+        // roll = Math.round(euler.roll*sensitivity_constant + 500);
 
-	    	serialPort.write("0" + pitch + "\r");
+        // console.log(pitch);
+        // console.log(roll);
 
-    	} else {
-	    	serialPort.write("0500\r");
-	    	serialPort.write("1500\r");
+        // serialPort.write("1" + roll + "\r");
 
-    	}
+        // serialPort.write("0" + pitch + "\r");
+
+      // } else {
+      //  serialPort.write("0500\r");
+      //  serialPort.write("1500\r");
+
+      // }
     });
 };
     
-setTimeout(socketWrite(sensitivity_constant),18);
+// setTimeout(socketWrite(sensitivity_constant),18);
